28171882a67afc267795b6e05bae244058de65d9
nsood1 <nbsood@mail.smu.edu>
2022-02-10 09:56:58 -0600

    Insertsssss

--- parking_schema.sql
+++ parking_schema.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE rodeo_stadium;
 
--- Table Creations
+---Create Tables :)
 
 CREATE TABLE stadium(
     id SERIAL PRIMARY KEY,
@@ -73,19 +73,15 @@ CREATE TABLE fan(
     ticket_num INTEGER REFERENCES ticket(ticket_num)
 );
 
--- Table Insertions
+-- Table Insertions :|
 
 INSERT INTO stadium(name, address, capacity)
-VALUES('Rodeo', '11059 Harry Hines', 80000);
-
-INSERT INTO stadium(name, address, capacity)
-VALUES('Cowpokes', '88 Main Street', 50000);
+VALUES('Rodeo', '11059 Harry Hines', 80000),
+('Cowpokes', '88 Main Street', 50000);
 
 INSERT INTO event(id, name, date, num_sold, stadium_id)
-VALUES (1, 'Dallas AnimeCon OwO', '3-25-22', 35000, 1);
-
-INSERT INTO event(id, name, date, num_sold, stadium_id)
-VALUES (2, 'Math Olympics! Proofs Welcome!', '7-16-22', 2000, 2);
+VALUES (1, 'Dallas AnimeCon OwO', '3-25-22', 35000, 1),
+ (2, 'Math Olympics! Proofs Welcome!', '7-16-22', 2000, 2);
 
 INSERT INTO parking_lot(num_spots, stadium_id)
 VALUES (3000, 1), (5000, 1), (3000, 2), (600, 2);
@@ -96,22 +92,28 @@ INSERT INTO schedule(schedule_num, start_time, end_time, lot_id, event_id) VALUE
 (3, '1:00:00', '11:00:00', 1, 2);
 
 INSERT INTO employee(id, name, schedule_num, lot_id) VALUES
-('Nicole', 'Sood', 1, 1), ('Trevor', 'Dohm', 1, 2),
-('Grace', 'McGinty', 2, 1), ('Josh', 'Govota', 3, 2);
+(1, 'Nicole Sood', 1, 1), (2, 'Trevor Dohm', 1, 2),
+(3, 'Grace McGinty', 2, 1), (4, 'Josh Govota', 3, 2);
 
--- FIXME: Add Whatever you want, the first couple of things look good to me!
--- INSERT INTO public.parkingspot(spotnum, lotid, isopen, isdisabled, event, employee) VALUES
--- (1, 1, false, true, 4, 1), (2, 1, true, false, 4, 1), (3, 1, true, false, 4, 1), (4, 1, true, false, 4, 1), (5, 1, false, false, 4, 1),
--- (1, 2, false, true, 5, 3), (2, 2, true, false, 5, 2), (3, 2, true, false, 6, 2), (4, 2, true, false, 5, 1), (5, 2, false, false, 5, 3);
---
--- INSERT INTO public.ticketinfomation(ticketnumber, numberofguests, eventattending, approvedparkinglot) VALUES
--- (1, 4, 4, 1), (2, 5, 5, 2), (3, 1, 5, 2);
---
--- INSERT INTO public.vehicle(licenseplate, type, tickernum) VALUES
--- ('MB56GH8', 'SUV', 1), ('A98B82W', 'Van', 1), ('G4JK901', 'Sudan', 3);
---
--- INSERT INTO public.shiftschedule(event, employee, lotplacement) VALUES
--- (4, 1, 1), (4, 2, 2), (4, 4, 1);
---
--- INSERT INTO public.spotassignement(spotassigned, liscenceplate) VALUES
--- (1, 'MB56GH8'), (10, 'A98B82W'), (8, 'G4JK901');
+INSERT INTO parking_spot(spot_num, is_available, is_handicap, employee_id, employee_name, lot_id) VALUES
+(101, false, true, 1, 'Nicole Sood', 1), (102, false, false, 1, 'Nicole Sood', '1'),
+(103, true, true, 1, 'Nicole Sood', 1), (104, true, false, 1, 'Nicole Sood', '1'),
+(105, true, true, 1, 'Nicole Sood', 1), (106, true, false, 1, 'Nicole Sood', '1')  ;
+
+INSERT INTO parking_spot(spot_num, is_available, is_handicap, employee_id, employee_name, lot_id) VALUES
+(201, false, true, 2, 'Trevor Dohm', 2), (202, false, false, 2, 'Trevor Dohm', 2),
+(203, false, true, 2, 'Trevor Dohm', 2), (204, false, false, 2, 'Trevor Dohm', 2),
+(205, false, true, 2, 'Trevor Dohm', 2), (206, false, false, 2, 'Trevor Dohm', 2);
+
+INSERT INTO vehicle(license, type, lot_id, spot_num) VALUES
+('MB34HGL', 'Van', 1, 101), ('LR64NDK', 'SUV', 2, 202),
+('NLF16LB', 'Truck', 1, 102), ('LB06NGL', 'RV', 2, 201);
+
+
+INSERT INTO public.ticket(ticket_num, price, event_id, lot_id) VALUES
+(1, 88.00, 1, 1), (2, 76.98, 1, 1), (3, 149.98, 2, 2);
+
+INSERT INTO public.fan(ssn, name, car_license, car_type, ticket_num) VALUES
+(1111, 'Elizabeth Windsor', 'NLF16LB', 'Truck', 1),
+(2222, 'Taylor Swift', 'MB34HGL', 'Van', 2),
+(3333, 'Harry Styles', 'LB06NGL', 'RV', 3);
