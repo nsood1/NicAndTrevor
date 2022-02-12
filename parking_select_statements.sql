01d533a2f7c750f44ebae7d192eeb64bfb3a2140
nsood1 <nbsood@mail.smu.edu>
2022-02-12 10:59:45 -0600

    updated selects - have fun with the rest...?

--- parking_select_statements.sql
+++ parking_select_statements.sql
@@ -14,12 +14,23 @@ SELECT spot_num FROM parking_spot
 WHERE is_available = false;
 
 -- 5:What parking spaces ended up being used for Event 1?
+SELECT spot_num, lot_id FROM parking_spot
+JOIN event e on parking_spot.lot_id = e.stadium_id AND e.id = 1
+AND parking_spot.is_available = false;
 
 -- 6:What parking spaces ended up being used for Event 2?
+SELECT spot_num, lot_id FROM parking_spot
+JOIN event e on parking_spot.lot_id = e.stadium_id AND e.id = 2
+AND parking_spot.is_available = false;
 
 -- 7:What employees occupy lot 2 at Rodeo Stadium?
+--TREVOR: i check i understood this one right pls....
+SELECT name, schedule_num FROM employee
+WHERE lot_id = 1;
 
 -- 8:How many Trucks have parked at any of my venues?
+SELECT count(*) FROM vehicle
+WHERE type = 'Truck';
 
 -- 9:How many of each vehicle type have parked at any of my venues?
 
