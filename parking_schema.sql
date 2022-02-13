732913ec4ef4d254c16f0b64996b3b5970f4bdb2
TrevorDohm <tdohm@smu.edu>
2022-02-12 21:18:35 -0600

    Completed Queries

--- parking_schema.sql
+++ parking_schema.sql
@@ -94,24 +94,24 @@ INSERT INTO schedule(schedule_num, start_time, end_time, lot_id, event_id) VALUE
 INSERT INTO employee(id, name, schedule_num, lot_id) VALUES
 (1, 'Nicole Sood', 1, 1), (2, 'Trevor Dohm', 1, 2),
 (3, 'Grace McGinty', 2, 1), (4, 'Josh Govota', 3, 2);
+-- Note: There is also a lot 3 and 4, defined above.
 
 INSERT INTO parking_spot(spot_num, is_available, is_handicap, employee_id, employee_name, lot_id) VALUES
-(101, false, true, 1, 'Nicole Sood', 1), (102, false, false, 1, 'Nicole Sood', '1'),
-(103, true, true, 1, 'Nicole Sood', 1), (104, true, false, 1, 'Nicole Sood', '1'),
-(105, true, true, 1, 'Nicole Sood', 1), (106, true, false, 1, 'Nicole Sood', '1')  ;
+(101, false, true, 1, 'Nicole Sood', 1), (102, false, false, 1, 'Nicole Sood', 1),
+(103, true, true, 1, 'Nicole Sood', 1), (104, true, false, 1, 'Nicole Sood', 1),
+(105, true, true, 1, 'Nicole Sood', 1), (106, true, false, 1, 'Nicole Sood', 1)  ;
 
 INSERT INTO parking_spot(spot_num, is_available, is_handicap, employee_id, employee_name, lot_id) VALUES
 (201, false, true, 2, 'Trevor Dohm', 2), (202, false, false, 2, 'Trevor Dohm', 2),
 (203, false, true, 2, 'Trevor Dohm', 2), (204, false, false, 2, 'Trevor Dohm', 2),
 (205, false, true, 2, 'Trevor Dohm', 2), (206, false, false, 2, 'Trevor Dohm', 2);
 
+INSERT INTO ticket(ticket_num, price, event_id, lot_id) VALUES
+(1, 88.00, 1, 1), (2, 76.98, 1, 1), (3, 149.98, 2, 2);
+
 INSERT INTO vehicle(license, type, lot_id, spot_num) VALUES
 ('MB34HGL', 'Van', 1, 101), ('LR64NDK', 'SUV', 2, 202),
 ('NLF16LB', 'Truck', 1, 102), ('LB06NGL', 'RV', 2, 201);
-
-
-INSERT INTO ticket(ticket_num, price, event_id, lot_id) VALUES
-(1, 88.00, 1, 1), (2, 76.98, 1, 1), (3, 149.98, 2, 2);
 
 INSERT INTO fan(ssn, name, car_license, car_type, ticket_num) VALUES
 (1111, 'Elizabeth Windsor', 'NLF16LB', 'Truck', 1),
--- parking_select_statements.sql
+++ parking_select_statements.sql
@@ -8,10 +8,11 @@ WHERE parking_lot.stadium_id = 2;
 -- 3:How many parking spaces are there at Cowpokes Stadium?
 SELECT COUNT(*) FROM parking_spot
 WHERE parking_spot.lot_id = 2;
+-- Note: Adding more Lots can be connected by OR statements.
 
 -- 4:What parking spaces are currently available for use?
 SELECT spot_num FROM parking_spot
-WHERE is_available = false;
+WHERE is_available = true;
 
 -- 5:What parking spaces ended up being used for Event 1?
 SELECT spot_num, lot_id FROM parking_spot
@@ -24,14 +25,23 @@ JOIN event e on parking_spot.lot_id = e.stadium_id AND e.id = 2
 AND parking_spot.is_available = false;
 
 -- 7:What employees occupy lot 2 at Rodeo Stadium?
---TREVOR: i check i understood this one right pls....
 SELECT name, schedule_num FROM employee
-WHERE lot_id = 1;
+WHERE lot_id = 2;
+-- Note: Stadium 1, Lot 2. This will be the one with 5000 Spots.
 
 -- 8:How many Trucks have parked at any of my venues?
-SELECT count(*) FROM vehicle
+SELECT COUNT(*) FROM vehicle
 WHERE type = 'Truck';
 
 -- 9:How many of each vehicle type have parked at any of my venues?
+SELECT type, COUNT(type) AS frequency
+FROM vehicle
+GROUP BY type
+ORDER BY COUNT(type) DESC;
+-- Note: Thanks https://www.c-sharpcorner.com/blogs/sql-query-to-find-out-the-frequency-of-each-element-in-a-column1!
 
+SELECT * FROM vehicle;
 -- 10: On average, how many vehicles come to an event?
+SELECT ((1.0 * (SELECT COUNT(*) FROM vehicle))) /
+(SELECT COUNT(*) FROM event) AS average;
+-- Note: The 1.0 Makes the output become a Float.
