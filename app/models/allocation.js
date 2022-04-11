18946c336b6512303ac845c670014f1048c7c867
nsood1 <nbsood@mail.smu.edu>
2022-04-11 14:41:10 -0500

    :(

---
+++ app/models/allocation.js
@@ -0,0 +1,32 @@
+const knex = require('../database/knex');
+
+const ALLOCATION_TABLE = 'vehicle';
+
+const allocationData = async (spot_num, license) => {
+    const query = knex(ALLOCATION_TABLE).where({ spot_num, license });
+    const result = await query;
+    return result;
+}
+
+
+const createNewVechicle = async (license, type, spot_num, lot_id) => {
+    console.log('Licesnse:', license);
+    console.log('type', type);
+    console.log('spot_num', spot_num);
+    console.log('lot_id', lot_id);
+
+
+    const query = knex(ALLOCATION_TABLE).insert({ license, type, spot_num, lot_id });
+    console.log('Raw query for insert a new vechicle:', query.toString());
+    const result = await query;
+
+    return result;
+};
+
+
+
+
+module.exports = {
+    allocationData,
+    createNewVechicle
+};
--- app/routes/allocation.js
+++ app/routes/allocation.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const Employee = require('../models/employee');
+const Employee = require('../models/allocation');
 
 /**
  * https://expressjs.com/en/guide/routing.html#express-router
@@ -13,14 +13,30 @@ router.post('/', async (req, res, next) => {
     try {
         const body = req.body;
         console.log(body);
-        const result = await req.models.user.createNewUser(body.email, body.password);
+        const result = await req.models.Employee.allocationData(body.spot_num, body.license);
         res.status(201).json(result);
     } catch (err) {
-        console.error('Failed to create new user:', err);
+        console.error('Failed :( ', err);
         res.status(500).json({ message: err.toString() });
     }
 
     next();
 })
+
+router.put('/', async (req, res, next) => {
+    try {
+        const body = req.body;
+        console.log(body);
+        const result = await req.models.Employee.createNewVechicle(body.license, body.type, body.spot_num, body.lot_id);
+        res.status(201).json(result);
+    } catch (err) {
+        console.error('Failed :( ', err);
+        res.status(500).json({ message: err.toString() });
+    }
+
+    next();
+})
+
+
 
 module.exports = router;
