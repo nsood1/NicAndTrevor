9cc7ea0a82851e01fad9b00c91b937bc3aeb5282
nsood1 <nbsood@mail.smu.edu>
2022-04-13 17:49:52 -0500

    try this...?

--- app/models/spots.js
+++ app/models/spots.js
@@ -3,6 +3,7 @@ const knex = require('../middleware/knex');
 const SPOTS_TABLE = 'parking_spot';
 const LOT_TABLE = 'parking_lot';
 
+
 const stadiumAllocation = async (stadium_id) => {
     const query = knex(LOT_TABLE).where({ stadium_id });
     const result = await query;
@@ -22,13 +23,23 @@ const avalAllocation = async (is_available) => {
 }
 
 const filterStadiumLot = async (stadium_id, lot_id) => {
+    //Option 1
     const query = knex(LOT_TABLE).where({ stadium_id }).join(SPOTS_TABLE).where({lot_id});
+
+    //Option 2
+    const query = knex(SPOTS_TABLE).join('parking_lot', 'parking_lot.stadium_id', 'parking_spot.lot_id' ).where({stadium_id, lot_id})
+   
     const result = await query;
     return result;
 } 
 
 const andOrAllocation = async(stadium_id, lot_id, is_available) => {
+    //option 1
     const query = knex(LOT_TABLE).where({ stadium_id }).join(SPOTS_TABLE).where({lot_id, is_available});
+
+    //option 2
+    const query = knex(SPOTS_TABLE).join('parking_lot', 'parking_lot.stadium_id', 'parking_spot.lot_id' ).where({stadium_id, lot_id, is_available})
+
     const result = await query;
     return result;
 }
