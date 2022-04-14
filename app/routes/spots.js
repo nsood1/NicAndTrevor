90ee72177c702e055682903aeeb5ec17f2a31637
TrevorDohm <78062983+TrevorDohm@users.noreply.github.com>
2022-04-13 20:00:19 -0500

    Finished Querying

--- app/routes/spots.js
+++ app/routes/spots.js
@@ -22,20 +22,33 @@ router.get('/spots', async (req, res, next) => {
 
     try {
 
-        if (query.stadium_id, query.lot_id, query.is_available) {
-            result = await Spots.andOrAllocation(query.stadium_id, query.lot_id, query.is_available);
-        } else if (query.stadium_id, query.lot_id) {
-            result = await Spots.filterStadiumLot(query.stadium_ID, query.lot_id);
-        } else if (query.stadium_id) { 
-            result = await Spots.stadiumAllocation(query.stadium_id);
-        } else if (query.lot_id) {
-            result = await Spots.lotAllocation(query.lot_id);
-        } else if (query.is_available) {
-            result = await Spots.avalAllocation(query.is_available);
-        } res.status(200).json(result); 
-
+        // Should Be 8 Queries (One For Each Combination)
+        if (query.stadium_id && query.lot_id && query.is_available) {
+            result = await Spots.stadiumLotAvailableQuery(query.stadium_id, query.lot_id, query.is_available);
+        } 
+        else if (query.stadium_id && query.lot_id) {
+            result = await Spots.stadiumLotQuery(query.stadium_id, query.lot_id);
+        } 
+        else if (query.stadium_id && query.is_available) {
+            result = await Spots.stadiumAvailableQuery(query.stadium_id, query.is_available);
+        } 
+        else if (query.lot_id && query.is_available) {
+            result = await Spots.lotAvailableQuery(query.lot_id, query.is_available);
+        } 
+        else if (query.stadium_id) { 
+            result = await Spots.stadiumQuery(query.stadium_id);
+        }
+        else if (query.lot_id) {
+            result = await Spots.lotQuery(query.lot_id);
+        } 
+        else if (query.is_available) {
+            result = await Spots.availableQuery(query.is_available);
+        } 
+        else { result = await Spots.noQuery(); }
+        
+        res.status(200).json(result); 
     } catch (err) {
-        res.status(401).json({ message: 'Failed Query' });
+        res.status(401).json({ message: 'Could Not Execute Query' });
     }
     next();
 })
